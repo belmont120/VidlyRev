0b90bd961413cc046e7a78474dfdbdeada0e237e
Xin Wang <belmont.xwang@gmail.com>
2017-11-06 16:23:43 +1100

    AddedUWP

---
+++ VidlyRev/VidlyRev.Tests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VidlyRev.Controllers;
+using System.Web.Mvc;
+
+namespace VidlyRev.Tests.Controllers
+{
+    [TestClass]
+    class CustomersControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DetailsWithValidId()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            ViewResult result = controller.Details(1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+
+        }
+
+        [TestMethod]
+        public void DetailsWithInvalidId()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            ViewResult result = controller.Details(0) as ViewResult;
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+    }
+}
