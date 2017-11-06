e2a1ac24172b6b61d43412cde1792ea0c44dd8aa
Xin Wang <belmont.xwang@gmail.com>
2017-11-06 16:11:56 +1100

    AddedXamarinPCL

--- VidlyRev/VidlyRev/Models/Customer.cs
+++ VidlyRev/VidlyRev/Models/Customer.cs
@@ -10,5 +10,9 @@ namespace VidlyRev.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
     }
 }
