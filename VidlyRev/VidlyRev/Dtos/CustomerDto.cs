5ee2b49536bcab60848ea26b35c5c18e92ada8a3
Xin Wang <belmont.xwang@gmail.com>
2017-11-07 14:57:44 +1100

    AddCustomerDto

---
+++ VidlyRev/VidlyRev/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using VidlyRev.Models;
+
+namespace VidlyRev.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Required]
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
